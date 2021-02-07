7439a5f84560ea9b4e212c346c22badd9fa1c4cf
“PROGRAMMER_SAYKAT” <saykatghosh79.gmail.com>
2021-02-08 00:47:10 +0600

    lecture_08

---
+++ lecture_08.go
@@ -0,0 +1,26 @@
+package main
+ 
+import "fmt"
+
+func main(){
+ 
+ //var name string = "MASTER-ACADEMY"
+ //var mynumber , roll int = 20, 10
+ //fmt.Println(name, mynumber, roll)
+ 
+ /*
+ var name string = "MASTER-ACADEMY"
+ var mynumber , roll int = 20, 10
+ fmt.Println(name, mynumber, roll)
+ */
+ 
+ //Golang এ char কে  rune বলা হয়। 
+ 
+ var i rune = 'a'
+ var age int = 10 
+ var result float32 = 50.33
+ fmt.Println(i, age, result)
+
+}
+
+
